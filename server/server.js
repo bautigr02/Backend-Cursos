d93320a5a3069a43004f3b2b6cb27f545094fb17
AlvaroR <alvaro.ro02g@gmail.com>
2026-02-06 11:28:03 -0300

    Refactorizacion a patron repository del courseController

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const bodyParser = require('body-parser');
+//const bodyParser = require('body-parser');
 const {cambiarEstadoCurso} = require('./controllers/courseController');
 
 const app = express();
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json()); 
 app.use(cors()); 
-app.use(bodyParser.json());
+//app.use(bodyParser.json());
 
 const userRoutes = require('./routes/userRoutes');
 app.use('/api', userRoutes);
@@ -25,24 +25,21 @@ app.use('/api', workshopRoutes);
 
 
 //Servidor
-app.listen(port, () => {
+app.listen(port, async () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
-  cambiarEstadoCurso()
-  .then(() => {
-    console.log('Estado de los cursos e inscripciones actualizados al iniciar el servidor.');
-  })
-  .catch((error) => {
-    console.error('Error al actualizar el estado de los cursos/inscripciones:', error);
-  });
-
-  setInterval(() => {
-    cambiarEstadoCurso()
-    .then(() => {
-      console.log('Estado de los cursos e inscripciones actualizados.');
-    })
-    .catch((error) => {
-      console.error('Error al actualizar el estado de los cursos/inscripciones:', error);
-    });
+  try {
+    await cambiarEstadoCurso();
+    console.log('Estado de los cursos e inscripciones actualizado automáticamente al iniciar el servidor.');
+  }catch (error) {
+    console.error('Error inesperado al actualizar el estado de los cursos/inscripciones:', error);
+  }
+  setInterval(async () => {
+    try{
+      await cambiarEstadoCurso();
+      console.log('Estado de los cursos e inscripciones actualizado automáticamente.');
+    } catch (error) {
+      console.error('Error inesperado al actualizar el estado de los cursos/inscripciones:', error);
+    }
   }, 86400000); // 24 horas en milisegundos
 });
 
